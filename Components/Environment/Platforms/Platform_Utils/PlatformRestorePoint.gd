ed392e9e12d062c5c08ed2dcd8732fa5abbaa6d2
Maxwell <maxwell@layer8games.com>
2024-01-27 18:20:05 -0800

    Milestone: Jumping is working now. It could probably use some more tweaking, but at least the numbers to tweak, and the general equation for how it should work is there.

--- Components/Environment/Platforms/Platform_Utils/PlatformRestorePoint.gd
+++ Components/Environment/Platforms/Platform_Utils/PlatformRestorePoint.gd
@@ -5,10 +5,8 @@ onready var restore_position : Vector2
 func _ready():
 	restore_position = global_position
 	restore_position.y -= 8
-	print("platform restore position is ", restore_position)
 
 
 func _on_Area2D_body_entered(body):
 	if body.has_method("set_restore_position"):
-		print("I am a player!")
 		body.set_restore_position(restore_position)
--- Entities/PlayerCharacters/Player_Character.gd
+++ Entities/PlayerCharacters/Player_Character.gd
@@ -1,30 +1,36 @@
 extends KinematicBody2D
 
 const UP : Vector2 = Vector2.UP
-const GRAVITY : float = 500.0
-const FAIL_PLANE : float = 1000.0
+const GRAVITY : float = 300.0
+const JUMP_GRAVITY : float = 100.0
+const TERMINAL_VELOCITY : float = 1_000.0
+const FAIL_PLANE : float = 1_000.0
 # Max speed the velocity can reach from acceleration
 # X values
 const walk_max_speed : float = 250.0
 const run_max_speed : float = 600.0
+const air_max_speed : float = 1_000.0
 # Y value
-const jump_max_speed : float = 500.0
+const jump_max_speed : float = 2_000.0
 # These are the acceleration values. These are appliec to the velocity each fram
 # to move the player
 # X values
 const walk_accel : float = 50.0
 const run_accel : float = 75.0
-const air_accel : float = 15.0
+const air_accel : float = 30.0
 # Y Values
-const jump_accel : float = 150.0
+const jump_accel : float = 1_000.0
+const initial_jump_force : float = 500.0
 # Rate of slow down, while on the ground
 const ground_decel : float = 75.0 
+const air_decel : float = 50.0
 
 onready var animatedSprite = $AnimatedSprite
 
 var move_velocity : Vector2 = Vector2.ZERO
 var restore_position : Vector2 = position
 var can_jump : bool = true
+var jumping : bool = false
 var in_air : bool
 
 
@@ -55,9 +61,10 @@ func move():
 					move_velocity.x = walk_max_speed
 			animatedSprite.play("walk")
 			animatedSprite.flip_h = false
-		else:
-			# Later, allow the player to move a little less. Create a new variable
-			pass
+		elif in_air:
+			move_velocity.x += air_accel
+			if move_velocity.x > air_max_speed:
+				move_velocity.x = air_max_speed
 	elif Input.is_action_pressed("p1_left"):
 		if not in_air:
 			if Input.is_action_pressed("p1_action"):
@@ -70,6 +77,11 @@ func move():
 					move_velocity.x = -walk_max_speed
 			animatedSprite.play("walk")
 			animatedSprite.flip_h = true
+		# This did not really work like I thought it would.
+		#elif in_air:
+		#	move_velocity.x -= air_accel
+		#	if move_velocity.x < air_max_speed:
+		#		move_velocity.x = -air_max_speed
 	else:
 		if not in_air:
 			if move_velocity.x > 0:
@@ -80,38 +92,56 @@ func move():
 			if move_velocity.x <= ground_decel and move_velocity.x >= -ground_decel:
 				move_velocity.x = 0
 				animatedSprite.play("idle")
+		elif in_air:
+			if move_velocity.x > 0:
+				move_velocity.x -= air_decel
+			elif move_velocity.x < 0:
+				move_velocity.x += air_decel
+			
 
 
 func jump():
-	if Input.is_action_pressed("p1_jump") and can_jump:
-		animatedSprite.play("jump_ready")
-	
-	if Input.is_action_just_released("p1_jump") and can_jump:
-		animatedSprite.play("jump_action")
-		#move_velocity.y = -jump_power
-		#can_jump = false
+	if Input.is_action_just_pressed("p1_jump"):
+		if can_jump and not in_air:
+			can_jump = false
+			jumping = true
+			move_velocity.y = -initial_jump_force
+			animatedSprite.play("jump")
+	elif Input.is_action_pressed("p1_jump") and jumping:
+		print("about to add extra jump, move_velocity.y is", move_velocity.y)
+		move_velocity.y -= jump_accel
+		if move_velocity.y <= -jump_max_speed:
+			jumping = false
+	elif Input.is_action_just_released("p1_jump"):
+		jumping = false
 
 
 func apply_gravity():
 	# falling, need gravity
 	if not is_on_floor():
-		move_velocity.y = GRAVITY
-		can_jump = false
+		if jumping:
+			move_velocity.y += JUMP_GRAVITY
+			print("gravity for jumping appliked, move_velocity y is ", move_velocity.y)
+		else:
+			move_velocity.y += GRAVITY
 		in_air = true
+		
+		#if move_velocity.y > TERMINAL_VELOCITY:
+		#	move_velocity.y =  TERMINAL_VELOCITY
 	# on floor. For testing we can jump. This needs to change soon
 	else:
+		if not can_jump:
+			print("hit floor, about to reset can_jump")
 		can_jump = true
 		in_air = false
+		#move_velocity.y = 0
 
 
 func check_for_fail():
 	if position.y >= FAIL_PLANE:
-		print("You kind of died! Setting position to ", restore_position)
 		position = restore_position
-		print("position is now ", position)
 
 
 func set_restore_position(restore_pos : Vector2):
-	print("restore position set to ", restore_pos)
 	restore_position = restore_pos
 
